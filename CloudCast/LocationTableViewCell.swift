9488db81a5bcda41b3f404aa8c116eafa148a292
ShahadatHossen_IOS <119600022+shahadat-100@users.noreply.github.com>
2025-01-12 01:30:46 +0600

    biggining

---
+++ CloudCast/LocationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LocationTableViewCell.swift
+//  CloudCast
+//
+//  Created by shahadat on 11/1/25.
+//
+
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
